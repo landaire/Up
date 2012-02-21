0bdc73cb3c4f1904ad01f2a7e9f1b0d0d214187b
Lander Brandt <landerbrandt@gmail.com>
2012-02-20 17:07:14 -0800

    Dialog is now multithreaded, progress shown

--- Up/progressdialog.cpp
+++ Up/progressdialog.cpp
@@ -1,12 +1,17 @@
 #include "progressdialog.h"
 #include "ui_progressdialog.h"
+#include <QThread>
+#include <QtConcurrentRun.h>
+#include "../FATX/StaticInformation.h"
 
-ProgressDialog::ProgressDialog(QWidget *parent, Operations Operation, std::string *Path, std::string OutPath, Drive *Drive) :
+ProgressDialog::ProgressDialog(QWidget *parent, Operations Operation, std::string *Path, std::string OutPath, Drive *drive) :
     QDialog(parent),
     ui(new Ui::ProgressDialog)
 {
     ui->setupUi(this);
-    PerformOperation(Operation, Path, OutPath, Drive);
+
+    QtConcurrent::run(this, &ProgressDialog::PerformOperation, Operation, *Path, OutPath, drive);
+    operation = Operation;
 }
 
 ProgressDialog::~ProgressDialog()
@@ -14,12 +19,12 @@ ProgressDialog::~ProgressDialog()
     delete ui;
 }
 
-void ProgressDialog::PerformOperation(Operations Operation, std::string *Path, std::string OutPath, Drive *Drive)
+void ProgressDialog::PerformOperation(Operations Operation, std::string Path, std::string OutPath, Drive *Drive)
 {
     switch(Operation)
     {
     case OperationCopyToDisk:
-        CopyFileToLocalDisk(Path, OutPath, Drive); //QtConcurrent::run(CopyFileToLocalDisk, Path, OutPath);
+        CopyFileToLocalDisk(&Path, OutPath, Drive);
         break;
     }
 }
@@ -27,5 +32,18 @@ void ProgressDialog::PerformOperation(Operations Operation, std::string *Path, s
 void ProgressDialog::CopyFileToLocalDisk(std::string *Path, std::string OutPath, Drive *Drive)
 {
     std::string InPath = *Path;
+    qRegisterMetaType<Progress>();
+    connect(Drive, SIGNAL(FileProgressChanged(const Progress&)), this, SLOT(OnFileProgressChanged(const Progress&)), Qt::QueuedConnection);
     Drive->CopyFileToLocalDisk(InPath, OutPath);
+}
+
+void ProgressDialog::OnFileProgressChanged(const Progress& p)
+{
+    if (ui->progressCurrent->maximum() != p.Maximium)
+        ui->progressCurrent->setMaximum(p.Maximium);
+
+    ui->progressCurrent->setValue(p.Current);
+
+    if (ui->groupBoxCurrent->title().toStdString() != p.FilePath)
+        ui->groupBoxCurrent->setTitle(QString::fromStdString(p.FilePath));
 }
